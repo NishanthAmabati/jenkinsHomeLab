26f448ce54569c7cb8bf445d32da2d1cd701eb3e
Nishanth Ambati <yagnya.nishanth@gmail.com>
2024-09-02 10:08:02 +0530

    dud

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -16,9 +16,10 @@ public class HelloServlet extends HttpServlet {
         response.getWriter().println("<h1>Hello, World!</h1>");
 	response.getWriter().println("<p>Let's Learn DevOps!</p>");
 	response.getWriter().println("<p>But first what is DevOps></p>");
-	response.getWriter().println("<p>Change 1></p>");
-	response.getWriter().println("<p>Change 2></p>");
-	response.getWriter().println("<p>Change 3></p>");
+	response.getWriter().println("<p>Change 1</p>");
+	response.getWriter().println("<p>Change 2</p>");
+	response.getWriter().println("<p>Change 3</p>");
+	response.getWriter().println("<p>Hi, Dud!</p>");
     }
 }
 
