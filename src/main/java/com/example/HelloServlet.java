0162f4ee8c702c8152cd9da0a8b2434516976e37
Nishanth Ambati <yagnya.nishanth@gmail.com>
2024-09-02 09:54:19 +0530

    added something

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -16,6 +16,7 @@ public class HelloServlet extends HttpServlet {
         response.getWriter().println("<h1>Hello, World!</h1>");
 	response.getWriter().println("<p>Let's Learn DevOps!</p>");
 	response.getWriter().println("<p>But first what is DevOps></p>");
+	response.getWriter().println("<p>Change 1></p>");
     }
 }
 
